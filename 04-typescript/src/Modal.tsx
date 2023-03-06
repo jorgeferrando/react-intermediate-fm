ff822d99740cb26f3846f6dcd5c8a61883179a03
Jorge Ferrando <jorgeparent@gmail.com>
2023-03-06 17:30:17 +0100

    modal to typescript

--- 04-typescript/src/Modal.tsx
+++ 04-typescript/src/Modal.tsx
@@ -1,8 +1,8 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, MutableRefObject, ReactElement } from "react";
 import { createPortal } from "react-dom";
 
-const Modal = ({ children }) => {
-    const elRef = useRef(null);
+const Modal = ({ children }: { children: ReactElement }) => {
+    const elRef: MutableRefObject<HTMLDivElement | null> = useRef(null);
     // if you initialize useRef() to document.createElement("div") by default it will be slower
     // all that starts with "document dot" it is slow and unperformant
     // so create arbitrary number of divs and cast away is not a good use of the CPU
@@ -12,10 +12,17 @@ const Modal = ({ children }) => {
 
     useEffect(() => {
         const modalRoot = document.getElementById("modal");
+        if (!modalRoot || !elRef.current) {
+            return;
+        }
         modalRoot.appendChild(elRef.current);
 
         // all returned in an effect will run when component unmounts
-        return () => modalRoot.removeChild(elRef.current);
+        return () => {
+            if (elRef.current) {
+                modalRoot.removeChild(elRef.current);
+            }
+        };
     }, []);
 
     return createPortal(<div>{children}</div>, elRef.current);
