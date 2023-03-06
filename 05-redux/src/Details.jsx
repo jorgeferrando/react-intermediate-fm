25958d6f81fc81ab2310744f8101ff9decfa7fc6
Jorge Ferrando <jorgeparent@gmail.com>
2023-03-06 19:17:53 +0100

    use createApi to run api calls

--- 05-redux/src/Details.jsx
+++ 05-redux/src/Details.jsx
@@ -1,9 +1,8 @@
 import { useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
 import { useDispatch } from "react-redux";
+import { useGetPetQuery } from "./PetApiService";
 import { adopt } from "./AdoptedPetSlice";
-import fetchPet from "./fetchPet";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
 import Modal from "./Modal";
@@ -12,18 +11,16 @@ const Details = () => {
     const [showModal, setShowModal] = useState(false);
     const navigate = useNavigate();
     const { id } = useParams();
-    const results = useQuery(["details", id], fetchPet);
+    const { isLoading, data: pet } = useGetPetQuery(id);
     const dispatch = useDispatch();
 
-    if (results.isLoading) {
+    if (isLoading) {
         return (
             <div className="loading-pane">
                 <h2 className="loader">🧿</h2>
             </div>
         );
     }
-
-    const pet = results.data.pets[0];
 
     return (
         <div className="details">
--- 05-redux/src/store.js
+++ 05-redux/src/store.js
@@ -1,11 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import adoptedPet from "./AdoptedPetSlice";
+import { petApi } from "./PetApiService";
 import searchParams from "./SearchParamsSlice";
 const store = configureStore({
     reducer: {
         adoptedPet,
         searchParams,
+        [petApi.reducerPath]: petApi.reducer,
     },
+    middleware: (
+        getDefaultMiddlewere // allows redux to cache
+    ) => getDefaultMiddlewere().concat(petApi.middleware),
 });
 
 export default store;
