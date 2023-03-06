fbcbef7e8c5d0d8a0cfb79522de1dae4a4d158ca
Jorge Ferrando <jorgeparent@gmail.com>
2023-03-06 19:18:23 +0100

    fix typo

--- 05-redux/src/PetApiService.js
+++ 05-redux/src/PetApiService.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const petApi = createApi({
     reducerPath: "petApi",
     baseQuery: fetchBaseQuery({ baseUrl: "http://pets-v2.dev-apis.com" }),
-    endPoints: (builder) => ({
+    endpoints: (builder) => ({
         getPet: builder.query({
             query: (id) => ({ url: "pets", params: { id } }),
             transformResponse: (response) => response.pets[0],
