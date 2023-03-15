27573618cdeb7912f5b398bb1949bbf17147358b
Jorge Ferrando <jorgeparent@gmail.com>
2023-03-15 13:51:33 +0100

    test useBreedList hook

---
+++ 06-testing/src/__test__/useBredList.test.jsx
@@ -0,0 +1,48 @@
+import { expect, test } from "vitest";
+import { render, renderHook } from "@testing-library/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import useBreedList from "../useBreedList";
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        staleTime: Infinity,
+        cacheTime: Infinity,
+        retry: false,
+    },
+});
+
+function getBreedList(animal) {
+    let list;
+
+    function TestComponent() {
+        list = useBreedList(animal);
+        return null;
+    }
+
+    render(
+        <QueryClientProvider client={queryClient}>
+            <TestComponent />
+        </QueryClientProvider>
+    );
+
+    return list;
+}
+
+test("gives an empty list with no animals provided old way", async () => {
+    const [breedList, status] = getBreedList();
+    expect(breedList).toHaveLength(0);
+    expect(status).toBe("loading");
+});
+
+test("gives an empty list with no animals provided today", async () => {
+    const { result } = renderHook(() => useBreedList(""), {
+        wrapper: ({ children }) => (
+            <QueryClientProvider client={queryClient}>
+                {children}
+            </QueryClientProvider>
+        ),
+    });
+    const [breedList, status] = result.current;
+    expect(breedList).toHaveLength(0);
+    expect(status).toBe("loading");
+});
